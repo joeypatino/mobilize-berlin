bc0a5fae233ded59a0b9a7d58246b52937e3e215
allilengyi <allilengyi@riseup.net>
2021-09-03 11:43:11 +0200

    modified content of impressum.vue

--- js/src/views/About/Impressum.vue
+++ js/src/views/About/Impressum.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="container section">
     <h2 class="title">Impressum</h2>
-    <div class="content">Impressumspflicht. meeeh.</div>
+    <div class="content">
+      <h4>nach &#167; 18 MStV</h4>
+      <p>
+        Die Mobilizon-Instanz wird betrieben von
+        <br /><br />
+        Thomas Frenzel<br />
+        Köpenicker Landstraße 95<br />
+        12437 Berlin
+      </p>
+      <p>
+        Bei Fragen könnt ihr das
+        <a href="https://kontakt.mobilize.berlin" target="_blank"
+          >Kontaktformular</a
+        >
+        verwenden oder ihr schreibt an
+        <a href="mailto:kontakt@mobilize.berlin">kontakt@mobilize.berlin</a>
+      </p>
+    </div>
   </div>
 </template>
 
