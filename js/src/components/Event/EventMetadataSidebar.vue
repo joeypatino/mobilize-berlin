8d3d4772872b4e03edff24ecddf3bd3bf420248d
allilengyi <allilengyi@riseup.net>
2021-09-07 22:45:49 +0200

    add "zoom-in" cursor when hovering over the image in sidebar

--- js/src/components/Event/EventMetadataSidebar.vue
+++ js/src/components/Event/EventMetadataSidebar.vue
@@ -136,7 +136,7 @@
     <div v-if="event.picture">
       <h2>{{ $t("Headline picture") }}</h2>
       <div style="position: relative" @click="showImage = true">
-        <img :src="event.picture.url" style="width: 100%" />
+        <img :src="event.picture.url" style="width: 100%; cursor: zoom-in" />
       </div>
     </div>
     <b-modal v-if="event.picture" :active.sync="showImage" has-modal-card>
